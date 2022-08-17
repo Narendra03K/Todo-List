f3e7b27b38fa3479efdd1e3e24d3aed99fc95f6d
Code with Narendra <nickk2448@gmail.com>
2022-08-17 15:23:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+function App() {
+    return (
+        <div>
+            <Navbar></Navbar>
+        </div>
+    );
+}
+
+export default App;
